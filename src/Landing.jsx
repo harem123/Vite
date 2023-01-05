aac37076dd4f7f939d4f763af60de633003949f2
Camilo Portilla <31325055+harem123@users.noreply.github.com>
2023-01-05 13:53:23 -0500

    landing created

---
+++ src/Landing.jsx
@@ -0,0 +1,29 @@
+import React from "react"
+
+
+
+function Landing() {
+  
+
+  return (
+
+    <div className='container'>
+      <div className='row'>
+        <div className='col'>
+        <div className="px-6 py-7 my-7 text-center">
+           <h1 class="display-5 fw-bold">Goalab Training</h1>
+          <div class="col-lg-5 mx-auto">
+            <p class="lead mb-4">Mejora tu tecnica de juego 10 veces mas rapido</p>
+            <div class="g-grip gap-2 d-sm-flex justify-content-sm-center">
+              <button type="button" class="btn btn-primary btn-lg px-4">Ingresa</button>
+            </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    
+  )
+}
+
+export default Landing
