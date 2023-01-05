188cb2e4db2b63ec48bd48181ed3998d5addb022
Camilo Portilla <31325055+harem123@users.noreply.github.com>
2023-01-05 13:47:46 -0500

    update main

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min";
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import './index.css'
+import {BrowserRouter,Routes,Route} from "react-router-dom"
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
 )
