829100343c897978f572a51e93b7d6ec5376eeea
Camilo Portilla <31325055+harem123@users.noreply.github.com>
2023-01-05 13:49:48 -0500

    create atom module

---
+++ src/Atom.jsx
@@ -0,0 +1,27 @@
+import React from "react"
+
+function Atom() {
+  
+
+  return (
+
+    <div className='container'>
+      <div className='row'>
+        <div className='col'>
+          <div className="px-6 py-7 my-7 text-center">
+          <h1 className="display-5 fw-bold">ATOM SAS</h1>
+          <div className="col-lg-5 mx-auto">
+            <p class="lead mb-">Desarrolladora de Software Hacemos realidad tus Proyectos</p>
+            <div className="g-grip gap-2 d-sm-flex justify-content-sm-center">
+              <button type="button" class="btn btn-primary btn-lg px-4">Contactar</button>
+            </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    
+  )
+}
+
+export default Atom
